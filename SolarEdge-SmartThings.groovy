63c8d96f32901e727877765fe7e75f19ebafd036
zprius <zprius@users.noreply.github.com>
2017-02-07 00:46:41 -0500

    Update SolarEdge-SmartThings.groovy

--- SolarEdge-SmartThings.groovy
+++ SolarEdge-SmartThings.groovy
@@ -25,82 +25,82 @@ preferences {
 
 metadata {
 	definition (name: "Solaredge System", namespace: "strelitzia123", author: "David Tucker") {
-	capability "Power Meter"
-  capability "Energy Meter"
-  capability "Refresh"
-	      
-  attribute "energy_today", "string"
-	attribute "energy_month", "number"
-	attribute "energy_year", "number"
-	attribute "energy_lifetime", "number"
-  attribute "production_level", "number"
-    
-        
-  fingerprint deviceId: "Solaredge"
-}
+		capability "Power Meter"
+		capability "Energy Meter"
+		capability "Refresh"
 
-simulator {
-		// TODO: define status and reply messages here
-}
-
-tiles {
-  valueTile("solar", "device.power") {
-    state("solarPower", label: '${currentValue}\nSolar', unit:"W", backgroundColors: [
-          [value: 2, color: "#32CD32"],
-          [value: 1, color: "#000000"],
-        ]
-      )
-  }
-
-  valueTile("energy_today", "device.energy_today") {
-    state("energy_today", label: '${currentValue}\nToday', unit:"KWh", backgroundColors: [
-          [value: 1, color: "#bc2323"],
-          [value: 2, color: "#d04e00"],
-          [value: 5, color: "#f1d801"],
-          [value: 10, color: "#90d2a7"],
-      [value: 15, color: "#44b621"],
-          [value: 20, color: "#1e9cbb"],
-          [value: 25, color: "#153591"]
-        ]
-    )
-  }
-
-  valueTile("energy_month", "device.energy_month") {
-     state("energy_month", label: '${currentValue}\nMonth', unit:"KWh", backgroundColors: [
-            [value: 30, color: "#bc2323"],
-            [value: 60, color: "#d04e00"],
-            [value: 150, color: "#f1d801"],
-            [value: 300, color: "#90d2a7"],
-        [value: 450, color: "#44b621"],
-            [value: 600, color: "#1e9cbb"],
-            [value: 750, color: "#153591"]
-          ]
-      )
-   }
-
-  valueTile("energy_year", "device.energy_year") {
-    state("energy_year", label: '${currentValue}\nYear', unit:"MWh")
-  }
-  valueTile("energy_lifetime", "device.energy_lifetime") {
-    state("energy_lifetime", label: '${currentValue}\nLifetime', unit:"MWh")
-  }
-  valueTile("productionLevel", "device.production_level") {
-    state("productionLevel", label: '${currentValue}%\nProduction', unit:"%", backgroundColor: "#0000FF")
-  }
+		attribute "energy_today", "string"
+		attribute "energy_month", "number"
+		attribute "energy_year", "number"
+		attribute "energy_lifetime", "number"
+		attribute "production_level", "number"
 
 
-  standardTile("refresh", "device.energy_today", inactiveLabel: false, decoration: "flat") {
-      state "default", action:"polling.poll", icon:"st.secondary.refresh"
-  }
+		fingerprint deviceId: "Solaredge"
+	}
 
-        
-  main (["solar"])
+	simulator {
+			// TODO: define status and reply messages here
+	}
 
-  details(["solar", 
-  //        "productionLevel", 															// TODO coding for production level %
-          "energy_today", "energy_month", "energy_year", "energy_lifetime","refresh"])
+	tiles {
+		valueTile("solar", "device.power") {
+			state("solarPower", label: '${currentValue}\nSolar', unit:"W", backgroundColors: [
+			  [value: 2, color: "#32CD32"],
+			  [value: 1, color: "#000000"],
+			]
+			  )
+		}
 
-}
+		valueTile("energy_today", "device.energy_today") {
+		state("energy_today", label: '${currentValue}\nToday', unit:"KWh", backgroundColors: [
+		  [value: 1, color: "#bc2323"],
+		  [value: 2, color: "#d04e00"],
+		  [value: 5, color: "#f1d801"],
+		  [value: 10, color: "#90d2a7"],
+		  [value: 15, color: "#44b621"],
+		  [value: 20, color: "#1e9cbb"],
+		  [value: 25, color: "#153591"]
+		]
+		)
+		}
+
+	  valueTile("energy_month", "device.energy_month") {
+	     state("energy_month", label: '${currentValue}\nMonth', unit:"KWh", backgroundColors: [
+		    [value: 30, color: "#bc2323"],
+		    [value: 60, color: "#d04e00"],
+		    [value: 150, color: "#f1d801"],
+		    [value: 300, color: "#90d2a7"],
+		[value: 450, color: "#44b621"],
+		    [value: 600, color: "#1e9cbb"],
+		    [value: 750, color: "#153591"]
+		  ]
+	      )
+	   }
+
+	  valueTile("energy_year", "device.energy_year") {
+	    state("energy_year", label: '${currentValue}\nYear', unit:"MWh")
+	  }
+	  valueTile("energy_lifetime", "device.energy_lifetime") {
+	    state("energy_lifetime", label: '${currentValue}\nLifetime', unit:"MWh")
+	  }
+	  valueTile("productionLevel", "device.production_level") {
+	    state("productionLevel", label: '${currentValue}%\nProduction', unit:"%", backgroundColor: "#0000FF")
+	  }
+
+
+	  standardTile("refresh", "device.energy_today", inactiveLabel: false, decoration: "flat") {
+	      state "default", action:"polling.poll", icon:"st.secondary.refresh"
+	  }
+
+
+	  main (["solar"])
+
+	  details(["solar", 
+	  //        "productionLevel", 															// TODO coding for production level %
+		  "energy_today", "energy_month", "energy_year", "energy_lifetime","refresh"])
+
+	}
 }
 
 def installed() {
